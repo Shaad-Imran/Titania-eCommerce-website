6f95a1aeff6b91157a8e156524ac87491881c8c4
Imran <imranmansuri038@gmail.com>
2022-10-14 17:35:00 +0530

    mobile navbar added

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ const toastCloseBtn = document.querySelector(".toast-close-btn");
 toastCloseBtn.addEventListener("click", () => {
   notificationToast.classList.add("closed");
 });
+
+// mobile menu variables
+const mobileMenuOpenBtn = document.querySelectorAll(
+  "[data-mobile-menu-open-btn]"
+);
+const mobileMenu = document.querySelectorAll("[data-mobile-menu]");
+const mobileMenuCloseBtn = document.querySelectorAll(
+  "[data-mobile-menu-close-btn]"
+);
+const overlay = document.querySelector(".overlay");
+
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+  const mobileMenuCloseFunc = function () {
+    mobileMenu[i].classList.remove("active");
+    overlay.classList.remove("active");
+  };
+
+  mobileMenuOpenBtn[i].addEventListener("click", function () {
+    mobileMenu[i].classList.add("active");
+    overlay.classList.add("active");
+  });
+
+  mobileMenuCloseBtn[i].addEventListener("click", mobileMenuCloseFunc);
+  overlay.addEventListener("click", mobileMenuCloseFunc);
+}
